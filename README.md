4994e4ac04ced7879ec47e0487e3e350ff9e9a52
TolstoyDotCom <sampro_domains@yahoo.com>
2018-09-03 17:21:59 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+To add to this listing, add rows to listing.csv, then run make-index.php to write index.html
+
+These files are from http://github.hubspot.com/sortable (released under the MIT license):
+css/sortable-theme-bootstrap.css
+css/sortable-theme-light.css
+js/sortable.min.js
+index.tpl.php
+
+This file is from https://github.com/jedfoster/Readmore.js/ (released under the MIT license):
+js/readmore.min.js
+
---
+++ make-index.php
@@ -0,0 +1,66 @@
+<?php
+
+function buildTableHeader( $line ) {
+	$s = '<tr>';
+	$ary = [];
+	$cellNumber = 0;
+	foreach ( $line as $data ) {
+		if ( !$cellNumber ) {
+			$extra = ' data-sortable-type="date" ';
+		}
+		else {
+			$extra = '';
+		}
+
+		$ary[] = '<th data-sortable="true" ' . $extra . '>' . $data . '</th>';
+		$cellNumber++;
+	}
+
+	return $s . implode( '', $ary ) . '</tr></thead>' . PHP_EOL;
+}
+
+function buildTableRow( $line ) {
+	$s = '<tr>';
+	$ary = [];
+	foreach ( $line as $data ) {
+		if ( stripos( $data, 'http' ) !== FALSE ) {
+			$data = str_replace( '.', ' &middot; ', str_ireplace( [ 'http://', 'https://' ], '', $data ) );
+		}
+		$classes = 'datacell';
+		if ( strlen( $data ) > 100 ) {
+			$data = '<div class="readmore">' . $data . '</div>';
+		}
+		$ary[] = '<td>' . $data . '</td>';
+	}
+
+	return $s . implode( '', $ary ) . '</tr>' . PHP_EOL;
+}
+
+$content = '<table class="sortable-theme-bootstrap" data-sortable><thead>';
+
+$fp = fopen( __DIR__ . '/listing.csv', 'r' );
+
+$lineNumber = 1;
+while ( $line = fgetcsv( $fp ) ) {
+	if ( $lineNumber == 1 ) {
+		$content .= buildTableHeader( $line );
+		$content .= '</thead><tbody>';
+	}
+	else {
+		$content .= buildTableRow( $line );
+	}
+	$lineNumber++;
+}
+
+$content .= '</tbody></table>';
+
+fclose( $fp );
+
+ob_start();
+include( __DIR__ . '/index.tpl.php' );
+$s = ob_get_contents();
+ob_end_clean();
+
+$fp = fopen( __DIR__ . '/index.html', 'w' );
+fwrite( $fp, $s );
+fclose( $fp );
